f7c524d23f19958676b534c05e9f9eb392f915c7
Faby252765 <fabiola.uribe@gmail.com>
2021-08-12 20:59:41 +0000

    Tarea Probada

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    edad=int(input("Dame tu edad: "))
+    actual=int(input("Dame el año actual: "))
+    calculo=100-edad
+    tiempo=actual+calculo
+    print("Cumplirás 100 años en el año:",tiempo)
 
 
 
--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    n = int(input("Dame la cantidad de juegos nuevos: "))
+    v = int(input("Dame la cantidad de juegos usados: "))
+    #Hacer el cálculo
+    resultado = (350 * v) + (1000 * n)
+    #Imprimir
+    print("El total de la compra es:",resultado)
+
 
 
 
